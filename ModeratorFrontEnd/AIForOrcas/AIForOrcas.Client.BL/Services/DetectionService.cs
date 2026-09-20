8053c05dcd33c121a8ea26df92e73c40a133c160
Dave Thaler <dthaler1968@gmail.com>
2026-09-19 22:59:58 -0700

    Display corrected detection timestamps in old epoch
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- ModeratorFrontEnd/AIForOrcas/AIForOrcas.Client.BL/Services/DetectionService.cs
+++ ModeratorFrontEnd/AIForOrcas/AIForOrcas.Client.BL/Services/DetectionService.cs
@@ -1,5 +1,8 @@
 ﻿using AIForOrcas.DTO;
 using AIForOrcas.DTO.API;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -19,12 +22,145 @@ namespace AIForOrcas.Client.BL.Services
         private readonly IAuthTokenProvider _authTokenProvider;
         private readonly ILogger<DetectionService> _logger;
 
+        /// <summary>
+        /// Start of current timestamp epoch.  Any detection with a timestamp
+        /// after this is assumed to be correct and does not need to be fixed.
+        /// </summary>
+        private readonly DateTime _currentEpochStart = new DateTime(2025, 10, 12, 14, 23, 00, DateTimeKind.Utc);
+
         public DetectionService(IHttpClientFactory httpClientFactory, IAuthTokenProvider authTokenProvider, ILogger<DetectionService> logger)
         {
             _httpClientFactory = httpClientFactory;
             _authTokenProvider = authTokenProvider;
             _logger = logger;
         }
+
+        Dictionary<string, List<string>> _s3FoldersCache = new Dictionary<string, List<string>>();
+
+        /// <summary>
+        /// Get the list of folders (representing .ts segment start times) for
+        /// a given location ID from the public S3 bucket.
+        /// </summary>
+        /// <param name="locationIdString">Location ID to look in</param>
+        /// <returns>List of folder names representing HLS start times</returns>
+        public async Task<List<string>> GetPublicS3FoldersAsync(string locationIdString)
+        {
+            // First try using a cached value.
+            if (_s3FoldersCache.TryGetValue(locationIdString, out var cachedFolders))
+            {
+                return cachedFolders;
+            }
+
+            var config = new AmazonS3Config
+            {
+                RegionEndpoint = RegionEndpoint.USWest2
+            };
+
+            using (var client = new AmazonS3Client(new Amazon.Runtime.AnonymousAWSCredentials(), config))
+            {
+                var allFolders = new List<string>();
+                string continuationToken = null;
+
+                do
+                {
+                    var request = new ListObjectsV2Request
+                    {
+                        BucketName = "audio-orcasound-net",
+                        Prefix = locationIdString + "/hls/",
+                        Delimiter = "/", // Group by folders
+                        ContinuationToken = continuationToken
+                    };
+
+                    var response = await client.ListObjectsV2Async(request);
+
+                    var folderNames = response.CommonPrefixes
+                        .Select(prefix => prefix.TrimEnd('/').Split('/').Last())
+                        .ToList();
+
+                    allFolders.AddRange(folderNames);
+
+                    continuationToken = response.IsTruncated ?? false
+                        ? response.NextContinuationToken
+                        : null;
+                } while (continuationToken != null);
+
+                _s3FoldersCache[locationIdString] = allFolders;
+                return allFolders;
+            }
+        }
+
+        /// <summary>
+        /// Check the list of detections for any with incorrect timestamps and
+        /// update the list with corrected timestamps.
+        /// </summary>
+        /// <param name="detections">List of detections to process</param>
+        public async Task FixTimestampsAsync(List<Detection> detections)
+        {
+            foreach (var detection in detections)
+            {
+                // Get the timestamp that OrcaHello listed in the detection.
+                // This is what was used to find the clips, but does not align
+                // with the start of a clip.
+                DateTime? originalDateTime = detection.Timestamp;
+                if (originalDateTime == null)
+                {
+                    continue;
+                }
+
+                // If the timestamp is in the current epoch, it is already correct.
+                if ((_currentEpochStart != null) && (originalDateTime >= _currentEpochStart))
+                {
+                    continue;
+                }
+
+                // Convert dateTime to Unix time (seconds since 1970-01-01).
+                var originalDateTimeOffset = new DateTimeOffset(originalDateTime.Value);
+                long originalUnixTimeSeconds = originalDateTimeOffset.ToUnixTimeSeconds();
+
+                // Fix the Unix time.  The originalDateTime is incorrect and
+                // was computed based on clips being 11 seconds long instead
+                // of 10 seconds long.  It is also based on the audio stream
+                // starting as of the .ts folder date, instead of about 2 seconds
+                // afterwards.  We can correct these once we know the
+                // .ts folder date to start calculating the drift based on.
+
+                string locationIdString = detection.Location.Id;
+                if (string.IsNullOrEmpty(locationIdString))
+                {
+                    continue;
+                }
+
+                List<string> folders = await GetPublicS3FoldersAsync(locationIdString);
+
+                // Find the most recent folder older than originalUnixTimeSeconds.
+                long folderTimeSeconds = 0;
+                foreach (var folderName in folders)
+                {
+                    if (long.TryParse(folderName, out long unixTime))
+                    {
+                        if (unixTime <= originalUnixTimeSeconds && unixTime > folderTimeSeconds)
+                        {
+                            folderTimeSeconds = unixTime;
+                        }
+                    }
+                }
+                if (folderTimeSeconds == 0)
+                {
+                    // No folder found.
+                    continue;
+                }
+
+                // Compute the correct timestamp of the start of the 60 second clip.
+                long originalSecondsIntoFolder = originalUnixTimeSeconds - folderTimeSeconds;
+                long originalClipIndex = originalSecondsIntoFolder / 11;
+                long correctedSecondsIntoFolder = originalClipIndex * 10 + 2;
+                long correctedUnixTimeSeconds = folderTimeSeconds + correctedSecondsIntoFolder;
+                DateTimeOffset correctedDateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(correctedUnixTimeSeconds);
+                DateTime correctedDateTime = correctedDateTimeOffset.UtcDateTime;
+                detection.Timestamp = correctedDateTime;
+            }
+        }
+
 
         // Get detections based on passed view, pagination options, and filter options
         private async Task<PaginatedResponseDTO<List<Detection>>> GetDetectionsAsync(string viewName, PaginationOptionsDTO paginationOptions, IFilterOptions filterOptions)
@@ -69,9 +205,11 @@ namespace AIForOrcas.Client.BL.Services
 
                 try
                 {
+                    var detections = JsonSerializer.Deserialize<List<Detection>>(responseString, defaultJsonSerializerOptions);
+                    await FixTimestampsAsync(detections);
                     return new PaginatedResponseDTO<List<Detection>>
                     {
-                        Response = JsonSerializer.Deserialize<List<Detection>>(responseString, defaultJsonSerializerOptions),
+                        Response = detections,
                         TotalAmountPages = totalAmountPages,
                         TotalNumberRecords = totalNumberRecords,
                         TotalNumberMinutes = totalNumberMinutes
